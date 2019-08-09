c2b0040018da2d4558cb86ec475dae43ab44c4d5
Gregor Sondermeier <gregor.sondermeier.github@gmail.com>
2019-08-09 10:16:02 +0200

    step 10 - affected

--- todoapp/apps/todos/src/app/app.component.spec.ts
+++ todoapp/apps/todos/src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todos'`, () => {
+  it(`should have as title 'Todos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todos');
+    expect(app.title).toEqual('Todos');
   });
 
   it('should render title in a h1 tag', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to todos!'
+      'Todos'
     );
   });
 });
